3c9931f05054d5323cf715f63d09f86c415503ab
UjuiUjuMandan <125150101+UjuiUjuMandan@users.noreply.github.com>
2023-10-31 02:59:00 +0000

    Re-enable swipe open (#13)

--- app/src/main/kotlin/com/hippo/ehviewer/ui/MainActivity.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/MainActivity.kt
@@ -288,7 +288,7 @@ class MainActivity : EhActivity() {
                 },
                 drawerState = drawerState,
                 // Disabled for breaking custom swipe gestures
-                gesturesEnabled = drawerState.isOpen,
+                gesturesEnabled = !drawerLocked,
             ) {
                 val insets = buildWindowInsets {
                     set(
