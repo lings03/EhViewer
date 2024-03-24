df69e8538f1f8dfc3354b0dd32ab60ac984a9248
UjuiUjuMandan <125150101+UjuiUjuMandan@users.noreply.github.com>
2024-03-24 18:41:27 +0000

    Switch default DoH URL to Quad9

--- app/src/main/kotlin/com/hippo/ehviewer/Settings.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/Settings.kt
@@ -173,7 +173,7 @@ object Settings : DataStorePreferences(null) {
     var defaultDownloadLabel by stringOrNullPref("default_download_label", null)
     var displayName by stringOrNullPref("display_name", null)
     var avatar by stringOrNullPref("avatar", null)
-    var dohUrl by stringPref("doh_url", "https://common.dot.dns.yandex.net/dns-query")
+    var dohUrl by stringPref("doh_url", "https://dns11.quad9.net/dns-query")
     var language by stringPref("app_language", "system").observed { updateWhenLocaleChanges() }
     var cloudflareIp by stringOrNullPref("cloudflare_ip", "cdn.sstatic.net")
     var cloudflareIpOverride by boolPref("cloudflare_ip_override", false)
