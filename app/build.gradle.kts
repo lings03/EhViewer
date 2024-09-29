32b868613136cdf6cc95034148060d037934f7cc
UjuiUjuMandan <125150101+UjuiUjuMandan@users.noreply.github.com>
2024-09-29 13:20:49 +0000

    1.12.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,7 +86,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 180060
-        versionName = "1.12.1.1"
+        versionName = "1.12.1.2"
         versionNameSuffix = "-cc"
         buildConfigField("String", "RAW_VERSION_NAME", "\"$versionName${versionNameSuffix.orEmpty()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"$commitSha\"")
