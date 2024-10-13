c5d636e6cdbbf0c488eb1e3c14a7acf9100c153e
不甜的砂糖 <90336521+lings03@users.noreply.github.com>
2024-10-13 14:13:20 +0800

    fix okhttp

--- app/src/main/kotlin/com/hippo/ehviewer/ktbuilder/Okhttp.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ktbuilder/Okhttp.kt
@@ -11,13 +11,13 @@ inline fun httpClient(builder: OkHttpClient.Builder.() -> Unit): OkHttpClient =
 
 inline fun httpClient(
     client: OkHttpClient,
-    builder: OkHttpClient.Builder.() -> Unit
+    builder: OkHttpClient.Builder.() -> Unit,
 ): OkHttpClient = client.newBuilder().apply(builder).build()
 
 fun OkHttpClient.Builder.cache(
     directory: Path,
     maxSize: Long,
-    fileSystem: FileSystem = FileSystem.SYSTEM
+    fileSystem: FileSystem = FileSystem.SYSTEM,
 ): OkHttpClient.Builder {
     val cacheDirFile = File(directory.toString())
     return cache(Cache(cacheDirFile, maxSize))
